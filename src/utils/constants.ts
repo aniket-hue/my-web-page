cbb1e1ef739dc0a89cb82e0281a56257d0fc46a1
Aniket Saxena <aniket.saxena@blu-smart.com>
2023-11-26 19:55:06 +0530

    fix: change resume link

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -3,5 +3,5 @@ export { default as COLOR } from "../color.json";
 export const LINKS = {
   GITHUB: "https://www.github.com/aniket-hue",
   LINKED_IN: "https://www.linkedin.com/in/aniket98/",
-  RESUME: "https://rxresu.me/aniketsaxena1998/aniket-saxena",
+  RESUME: "https://rxresu.me/aniketsaxena1998/aniket-saxena-sse-2-yoe",
 };
